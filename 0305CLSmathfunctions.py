07b4fcf2d7b0a121c55c34af24eda3dc392e633d
SWAROOPNC <95061667+SWAROOPNC@users.noreply.github.com>
2022-03-05 12:55:08 +0530

    Add files via upload

---
+++ 0305CLSmathfunctions.py
@@ -0,0 +1,20 @@
+#math functions
+import math
+#import module
+a,b,c,d,e = 11,22.56764,33,27,-45
+
+print(a,b,c,d)
+
+print(max(a,b,c,d)) #finds max of element
+print(min(a,b,c,d))
+
+print(abs(e)) #absolute function
+
+print(pow(2,3)) # power
+print(round(b,2)) # Rounding for digits
+print(math.sqrt(25)) #sqrt
+print(math.exp(d))#exponential
+print(math.ceil(b))# cieling
+print(math.log(23,10))#log
+print(math.sin(0))#sin
+print(math.cos(0))#cos
