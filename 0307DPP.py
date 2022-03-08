bcc039017dbe50b2a7f5f6bc8546186f44771292
SWAROOPNC <95061667+SWAROOPNC@users.noreply.github.com>
2022-03-08 12:14:16 +0530

    Update 0307DPP.py

--- 0307DPP.py
+++ 0307DPP.py
@@ -25,3 +25,29 @@ if (indicator == True ) :
     print(" Sec High is prime ")
 else :
     print (" Sec High is not prime")
+    
+"""Alternate way"""
+a=[]
+len=int(input("enter the length of list:"))
+for i  in range(0,len):
+    y=int(input("enter the elements of the list:"))
+    a.append(y)
+print(a)
+b=max(a)
+print(b)
+a.remove(b)
+print(a)
+c=max(a)
+
+indicator = True
+x= c
+for i in range (2,x-1) :
+    if (x%i == 0 ) :
+        indicator = False
+    elif(x==1) :
+        indicator = True
+if (indicator == True ) :
+    print(" NUm is prime ")
+else :
+    print (" Num is not prime")
+    
