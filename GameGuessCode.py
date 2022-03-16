8a2c0639678f198130a5ce382db5d23d088beff7
SWAROOPNC <95061667+SWAROOPNC@users.noreply.github.com>
2022-03-16 10:47:03 +0530

    Add files via upload

---
+++ GameGuessCode.py
@@ -0,0 +1,20 @@
+def GuessGameModule() :
+    import random
+    print("Welcome to Number Guessing Game ")
+    n = int(input("Enter max range of number to guess"))
+
+    for i in range(0,5) :
+        l1 = random.randint(0, n)
+        print("Enter any number between 0 to ",n)
+        i1 = int(input())
+        if (i1 == l1) :
+            print("Congratulations you won game ")
+            break
+        else :
+            if (i1 > l1) :
+                print("Secret number is greater than ",i1)
+            if (i1 < l1):
+                print("Secret number is lesser than ", i1)
+            print("Try Again ")
+    else :
+        print("Your 5 attempts exhausted , Sorry , \n You can Restart the game")
