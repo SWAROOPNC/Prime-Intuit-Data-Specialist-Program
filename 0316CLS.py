f6cead61412f3580cbf8d47c83133d829878342c
SWAROOP N C <95061667+SWAROOPNC@users.noreply.github.com>
2022-03-16 13:33:22 +0530

    Add files via upload

---
+++ 0316CLS.py
@@ -0,0 +1,9 @@
+import random
+l1 = random.randint(2,10)
+print(l1)
+l2 = random.randrange(0,100,10)
+print(l2)
+l2 = random.randrange(5,100,10)
+print(l2)
+l2 = random.randrange(10,100,10)
+print(l2)
