4bdf13c4b38d7119b6b2689c15d6631ae27a0dcc
SWAROOP N C <95061667+SWAROOPNC@users.noreply.github.com>
2022-03-24 21:51:00 +0530

    Update 0314CLSLambda2FIlter.py

--- 0314CLSLambda2FIlter.py
+++ 0314CLSLambda2FIlter.py
@@ -36,5 +36,5 @@ print("sum = reduce(lambda x,y : x+y,l1)",sum)
 from functools import reduce
 """fib = lambda n : reduce(lambda x : x+x[-1]+x[-2]),range(n-2)),[0,1]
 print(fib(4))""" #not works
-fib = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]],range(n-2), [0, 1])
+fib = lambda n: reduce(lambda x, y : x+[x[-1]+x[-2]],range(n-2), [0, 1])
 print(fib(5))
