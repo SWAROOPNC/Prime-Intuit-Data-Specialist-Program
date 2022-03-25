700dde9304cb0ca9587cb83f8a9e8f08c5f8cf38
SWAROOP N C <95061667+SWAROOPNC@users.noreply.github.com>
2022-03-25 16:28:47 +0530

    Update 0314CLSLambda2FIlter.py

--- 0314CLSLambda2FIlter.py
+++ 0314CLSLambda2FIlter.py
@@ -1,3 +1,37 @@
+#lambda can take 1 expression , n number of arguments , no retuen , no declaration
+#anonymous not need
+
+Trans = lambda a,x,b : a*x+b
+print(Trans(2,4,5))
+#retuns 13
+
+#lambda powerful when used within a function
+def conversion(a,c) :
+    return (lambda x : (a*x)+c)
+mtr_km=conversion(0.001,0)
+km_mtr=conversion(1000,0)
+"""kg_gm
+kg_ton
+ton_kg"""
+print(mtr_km(2000))
+print(km_mtr(2))
+mtr_km=Trans(0.001,2000,0)
+print(mtr_km)
+#prints 2.0
+#less changed numbers put in ac
+#more often called in x
+
+# whwn lambda used with iterables , map , filter and reduce, for more ease of use
+#map will consider input l1 and performs function specified , map ensures for every value in l1
+#filter specifies logical operator checks True or false , returns value in l1 where there is True
+#it returns only true or false
+#reduce , 1,2,3,4,
+#3,3,4
+#6,4
+#10
+#retuens only one number , no need to declare it as list
+#from functions import reduce , just ude reduce
+#if just import functoools , use functools.reduce
 l1=[1,2,3,4,5,6,7,8,9]
 print("l1",l1)
 
