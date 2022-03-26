7a42fd2e1553c67c7de8d9aa06279da53ac9807a
SWAROOP N C <95061667+SWAROOPNC@users.noreply.github.com>
2022-03-26 10:34:31 +0530

    Create 0326cls.sql

---
+++ 0326cls.sql
@@ -0,0 +1,50 @@
+/*; SEMICOLON MUST , COMMA AS SEPERATORS*/
+SELECT
+    employee_id,
+    first_name,
+    salary,
+    ( salary * 12 ) AS annual_salary
+FROM
+    employees;
+
+SELECT
+    employee_id,
+    first_name,
+    salary,
+    ( salary * 12 ) AS "ANNUAL SALARY"
+FROM
+    employees;
+
+DESC departments;
+
+SELECT
+    *
+FROM
+    departments;
+    
+/* Ctrl+F7 to beautify sql script, CONTROL SPACE AFTER WRITING */
+SELECT
+    d.department_id,
+    d.department_name
+FROM
+    departments d;
+/*WORKS FOR ALIAS ALSO AND WO ALSO, FIRST WRITE SELECT FROM TABLENAME THEN COLUMN NAMES*/
+SELECT
+    department_id,
+    department_name,
+    manager_id
+FROM
+    departments
+ORDER BY
+    manager_id;
+
+SELECT
+    department_name
+FROM
+    departments
+WHERE
+    manager_id = 200;
+/*-- Alt " , for font STYLE variable , sql not case sensitive , go to line control enter run */
+SELECT
+    employee_id,first_name,salary
+FROM employees;
