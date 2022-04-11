17e125b355ebd746e93ef4f26d0318afd32d3aab
SWAROOP N C <95061667+SWAROOPNC@users.noreply.github.com>
2022-04-11 16:32:02 +0530

    Add files via upload

---
+++ 1104js.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Encoded JS Text ';
+alert("Welcome PopUp 1 ");
+/*alert("COmmenting ");*/
+// is also inline comment
+alert(" Hellowwww ")
