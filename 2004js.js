2ab7b95c2b2c90ca33b675bef06916d5712caa18
SWAROOP N C <95061667+SWAROOPNC@users.noreply.github.com>
2022-04-20 17:05:05 +0530

    Add files via upload

---
+++ 2004js.js
@@ -0,0 +1,63 @@
+var n1 = parseInt(prompt("Enter num1"));
+console.log(n1);
+var op = prompt("Enter operator ");
+console.log(op);
+var n2 = parseInt(prompt("Enter num2"));
+console.log(n2);
+
+if (op=='+')
+{
+  x=n1+n2;
+}
+else if (op=='-') {
+  x=n1-n2;
+}
+else if (op=='*') {
+  x=n1*n2;
+}
+else if (op=='/') {
+  x=n1/n2;
+}
+alert(n1+op+n2+"="+x);
+console.log(n1+op+n2+"="+x);
+switch (op) {
+  case '+':x=n1+n2;  break;
+  case '-':x=n1-n2;  break;
+  case '*':x=n1*n2;  break;
+  case '/':x=n1/n2;  break;
+  default:
+}
+alert(n1+op+n2+"="+x);
+console.log(n1+op+n2+"="+x);
+
+
+function Add() {
+  num1=parseInt(document.getElementById('num1').value);
+  num2=parseInt(document.getElementById('num2').value);
+  document.getElementById('result').innerHTML=num1+num2;
+}
+function Sub() {
+  num1=parseInt(document.getElementById('num1').value);
+  num2=parseInt(document.getElementById('num2').value);
+  document.getElementById('result').innerHTML=num1-num2;
+}
+function multiplyBy() {
+  num1=parseInt(document.getElementById('num1').value);
+  num2=parseInt(document.getElementById('num2').value);
+  document.getElementById('result').innerHTML=num1*num2;
+}
+function divideBy() {
+  num1=parseInt(document.getElementById('num1').value);
+  num2=parseInt(document.getElementById('num2').value);
+  document.getElementById('result').innerHTML=num1/num2;
+}
+function Min() {
+  num1=parseInt(document.getElementById('num1').value);
+  num2=parseInt(document.getElementById('num2').value);
+  document.getElementById('result').innerHTML=num1>num2;
+}
+function Max() {
+  num1=parseInt(document.getElementById('num1').value);
+  num2=parseInt(document.getElementById('num2').value);
+  document.getElementById('result').innerHTML=num1<num2;
+}
